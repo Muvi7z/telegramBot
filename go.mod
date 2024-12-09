661a6a82f60a3ee276bda8ed412252536537569e
Ochir.zha <ochir-zhamsuev@yandex.ru>
2024-12-09 10:39:59 +0800

    add mock

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22.6
 require (
 	github.com/Masterminds/squirrel v1.5.4
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/gojuno/minimock/v3 v3.4.3
 	github.com/golang/mock v1.6.0
 	github.com/lib/pq v1.10.9
 	github.com/pkg/errors v0.9.1
@@ -24,7 +25,6 @@ require (
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/kr/text v0.2.0 // indirect
 	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
 	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
 	github.com/mfridman/interpolate v0.0.2 // indirect
--- internal/model/messages/incoming_msg_test.go
+++ internal/model/messages/incoming_msg_test.go
@@ -1,24 +1,150 @@
 package messages
 
 import (
-	mocks "github.com/Muvi7z/telegramBot.git/internal/mocks/messages"
+	"context"
+	mocks_msg "github.com/Muvi7z/telegramBot.git/internal/mocks/messages"
+	"github.com/gojuno/minimock/v3"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
+
+func parseDate(date string) time.Time {
+	v, _ := time.ParseInLocation(dateFormat, "01.10.2022", time.UTC)
+	return v
+}
 
 func TestOnStartCommand_ShouldAnswerWithIntroMessage(t *testing.T) {
 	ctrl := gomock.NewController(t)
-	sender := mocks.NewMockMessageSender(ctrl)
-	model := New(sender)
+	defer ctrl.Finish()
 
-	sender.EXPECT().SendMessage("не знаю эту команду", int64(123))
+	m := minimock.NewController(t)
+	defer m.Finish()
 
-	err := model.IncomingMessage(Message{
-		Text:   "/start",
-		UserID: (123),
-	})
+	userID := int64(123)
 
-	assert.NoError(t, err)
+	testCases := []struct {
+		name    string
+		command string
+		amount  string
+		kopecks int64
+		title   string
+		date    interface{}
+		answer  string
+	}{
+		{
+			name:    "normal",
+			amount:  "100.0",
+			kopecks: 10000,
+			title:   "расход",
+			date:    parseDate("01.10.2022"),
+			command: "/add 100.0; расход; 01.10.2022",
+			answer:  "Расход добавлен: 100.00 RUB расход 01.10.2022",
+		},
+		{
+			name:    "without title",
+			amount:  "100.0",
+			kopecks: 10000,
+			title:   "",
+			date:    parseDate("01.10.2022"),
+			command: "/add 100.0; ;01.10.2022",
+			answer:  "Расход добавлен: 100.00 RUB  01.10.2022",
+		},
+		{
+			name:    "without date",
+			amount:  "100.0",
+			kopecks: 10000,
+			title:   "расход",
+			date:    gomock.Any(),
+			command: "/add 100.0; расход;",
+			answer:  "Расход добавлен: 100.00 RUB расход " + time.Now().UTC().Format(dateFormat),
+		},
+		{
+			name:    "only amount with semicolon",
+			amount:  "100.0",
+			kopecks: 10000,
+			title:   "",
+			date:    gomock.Any(),
+			command: "/add 100.0;",
+			answer:  "Расход добавлен: 100.00 RUB  " + time.Now().UTC().Format(dateFormat),
+		},
+		{
+			name:    "only amount without semicolon",
+			amount:  "100.0",
+			kopecks: 10000,
+			title:   "",
+			date:    gomock.Any(),
+			command: "/add 100.0",
+			answer:  "Расход добавлен: 100.00 RUB  " + time.Now().UTC().Format(dateFormat),
+		},
+		{
+			name:    "without amount",
+			amount:  "",
+			kopecks: 0,
+			title:   "расход",
+			date:    gomock.Any(),
+			command: "/add ; расход; 01.10.2022",
+			answer:  InvalidAmountMessage,
+		},
+		{
+			name:    "invalid amount",
+			amount:  "100.0.0",
+			kopecks: 0,
+			title:   "расход",
+			date:    gomock.Any(),
+			command: "/add 100.0.0; расход; 01.10.2022",
+			answer:  InvalidAmountMessage,
+		},
+		{
+			name:    "invalid date",
+			amount:  "100.0",
+			kopecks: 10000,
+			title:   "расход",
+			date:    gomock.Any(),
+			command: "/add 100.0; расход; 01.24.2022",
+			answer:  InvalidDateMessage,
+		},
+		{
+			name:    "invalid date format",
+			amount:  "100.0",
+			kopecks: 10000,
+			title:   "расход",
+			date:    gomock.Any(),
+			command: "/add 100.0; расход; 01.10.2022.0",
+			answer:  InvalidDateMessage,
+		},
+		{
+			name:    "empty command",
+			command: "/add",
+			date:    gomock.Any(),
+			answer:  InvalidCommandMessage,
+		},
+	}
 
+	baseCurrency := "RUB"
+	sender := mocks_msg.NewMockMessageSender(ctrl)
+	config := mocks_msg.NewMockConfigGetter(ctrl)
+	expenseDB := mocks_msg.NewMockExpenseDB(ctrl)
+	userDB := mocks_msg.NewMockUserDB(ctrl)
+	rateDB := NewRateStorageMock(m)
+	updater := mocks_msg.NewMockCurrencyExchangeRateUpdater(ctrl)
+	model := New(sender, rateDB)
+
+	config.EXPECT().GetBaseCurrency().Return(baseCurrency).AnyTimes()
+	userDB.EXPECT().GetDefaultCurrency(gomock.Any(), userID).Return(baseCurrency, nil).AnyTimes()
+	userDB.EXPECT().UserExist(gomock.Any(), gomock.Any()).Return(true).AnyTimes()
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			expenseDB.EXPECT().AddExpense(gomock.Any(), userID, tc.kopecks, tc.title, tc.date).Return(nil).AnyTimes()
+			sender.EXPECT().SendMessage(tc.answer, userID).Return(nil)
+
+			err := model.IncomingMessage(context.TODO(), Message{
+				Text:   tc.command,
+				UserID: userID,
+			})
+
+			assert.NoError(t, err)
+		})
+	}
 }
